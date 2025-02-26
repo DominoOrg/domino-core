e79979dfb8add75797280af363c93f94dc0a90b7
nunzioono <nunzioonorati@gmail.com>
2025-02-26 19:12:29 +0100

    Implemented new complexity evaluation function

--- src/graphs/classify.rs
+++ src/graphs/classify.rs
@@ -2,71 +2,98 @@ use crate::Puzzle;
 
 use super::common::get_n;
 
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+
+    use crate::{Puzzle, Tile};
+
+    use super::classify_puzzle;
+
+    fn mock_puzzle(n: usize, c: usize) -> Puzzle {
+      let l = if n % 2 == 0 {(n + 1) * (n + 2) / 2} else {(n + 1) * (n + 1) / 2};
+      let mut puzzle: Puzzle = vec![Some(Tile::from((0,0))); l];
+      let max_hole = (l as f32 - (n as f32 / 2.0).floor()) as usize;
+      let log_factor: f32 = match c {
+        1 => 1.0 / puzzle.len() as f32,
+        2 => 4.0 / 7.0,
+        3 => 6.0 / 7.0,
+        _ => 0.0
+      };
+      for i in 0..(max_hole as f32  * log_factor.sqrt()).ceil() as usize {
+        puzzle[i] = None;
+      }
+      puzzle
+    }
+
+    #[test]
+    fn classify_unit() {
+      for mock_c in 1..=3 {
+        let puzzle = mock_puzzle(6, mock_c);
+        let c = classify_puzzle(&puzzle);
+        assert_eq!(c, mock_c);
+      }
+    }
+
+}
+
 pub fn classify_puzzle(puzzle: &Puzzle) -> usize {
     let n = get_n(puzzle).expect("Puzzle does not have a valid length");
     let l = if n % 2 == 0 {(n + 1) * (n + 2) / 2} else {(n + 1) * (n + 1) / 2};
-    let max_hole = (l as f32 - (n as f32 / 2.0).floor()) as usize;
-    let max_contigous_hole = max_hole_length(puzzle) as usize;
-    let class = if max_contigous_hole < (max_hole * 4) / 7 {
-        1
-    } else if max_contigous_hole < (max_hole * 6) / 7 {
-        2
-    } else {
-        3
-    };
-    class
+    let max_hole = l as f32 - (n as f32 / 2.0).floor();
+    let holes: Vec<(usize, usize)> = detect_holes(puzzle);
+    let absolute_complexity = compute_complexity(holes, max_hole);
+    absolute_complexity
 }
 
-fn max_hole_length(puzzle: &Puzzle) -> usize {
-    let mut max_contigous_holes = 0;
-    let mut current_hole = 0;
-    puzzle
-        .iter()
-        .enumerate()
-        .for_each(|(i, tile)| {
-            if (
-                i > 0 && tile.is_none() && puzzle[i - 1].is_some()
-            ) || (
-                i == 0 && tile.is_none() && puzzle[puzzle.len() - 1].is_some()
-            ) {
-                current_hole = 1;
-            }
-
-            if tile.is_none() && puzzle[(i + 1) % puzzle.len()].is_none() {
-                current_hole += 1;
-            }
-
-            if (
-                i > 0 && tile.is_some() && puzzle[i - 1].is_none()
-            ) || (
-                i == 0 && tile.is_some() && puzzle[puzzle.len() - 1].is_none()
-            ) {
-                if current_hole > max_contigous_holes {
-                    max_contigous_holes = current_hole;
-                }
-                current_hole = 0;
-            }
-        });
-    max_contigous_holes
+fn compute_complexity(holes: Vec<(usize, usize)>, max_hole: f32) -> usize {
+  let number_of_holes_factor = 1.0 / ((holes.len() as f32).sqrt().sqrt().sqrt().sqrt().sqrt());
+  let length_factor = holes.into_iter().map(|hole| (hole.1.saturating_sub(hole.0) as f32 /max_hole as f32)
+  .powf(2.0)).sum::<f32>();
+  // println!("number_of_holes_factor: {number_of_holes_factor}, length_factor: {length_factor}");
+  let complexity = number_of_holes_factor * length_factor;
+  let c = match complexity {
+    c if c < 4.0 / 7.0 => 1,
+    c if c < 6.0 / 7.0 => 2,
+    _ => 3
+  };
+  c
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::{graphs::classify::max_hole_length, Tile};
+fn detect_holes(puzzle: &Puzzle) -> Vec<(usize, usize)> {
+  let invalid_index = puzzle.len()+1;
+  let holes: Vec<(usize, usize)> = puzzle
+    .iter()
+    .enumerate()
+    .scan((invalid_index, invalid_index), |current_hole, (i, &item)| {
+        let is_hole_start = item.is_none() &&
+            ((i > 0 && puzzle[i-1].is_some()) ||
+             (i == 0 && puzzle[puzzle.len() - 1].is_some()));
 
-
-    #[test]
-    fn test_hole_length() {
-        for n in [3,6] {
-            let l = if n%2==0 {(n + 1) * (n + 2) / 2} else {(n + 1) * (n + 1) / 2};
-            let puzzle = vec![Some(Tile(0,0));l];
-            for i in 1..(l - (n + 1)) {
-                let mut p = puzzle.clone();
-                for j in 1..i+1 {
-                    p[j] = None;                
-                }
-                assert_eq!(max_hole_length(&p), i);
-            }
+        if is_hole_start {
+            current_hole.0 = i;
         }
-    }
+
+        let is_hole_end = item.is_none() &&
+            ((i < puzzle.len() - 1 && puzzle[i+1].is_some()) ||
+             (i == puzzle.len() - 1 && puzzle[0].is_some()));
+
+        let mut result = None;
+        if is_hole_end {
+            current_hole.1 = i + 1;
+            if current_hole.0 != invalid_index {
+                result = Some(*current_hole);
+            }
+            *current_hole = (invalid_index, invalid_index);
+        }
+
+        // println!("{i} {is_hole_start} {is_hole_end} {current_hole:?}");
+
+        Some(result)
+    })
+    .filter_map(|x| x)
+    .collect();
+
+  holes
 }
